1c5ef5e425caa162a60f745248c5ee5c5a92f171
stephan mehnert <stephan.mehnert@vtt.fi>
2023-05-30 14:10:07 +0300

    update rest example

--- examples/rest_client_example.py
+++ examples/rest_client_example.py
@@ -1,29 +1,31 @@
 # Example usage of the client library
+import sys
 from flxtrd import FlexAPIClient
-from flxtrd import AuthPlugin, LoggingPlugin
+from flxtrd import AuthPlugin, ListDevices
+from flxtrd import User
+
 
 # Example usage of the client library
-# Creating an instance of the REST API client
-# rest_api_client = RestAPIClient("https://api.example.com")
-
-# # Adding plugins to the REST API client
-# rest_api_client.add_plugin(AuthPlugin(app_token="YOUR_APP_TOKEN"))
-# rest_api_client.add_plugin(LoggingPlugin())
-
-# # Making requests using the REST API client
-# rest_response = rest_api_client.make_request("GET", "/users/")
-# print(rest_response.json())
 
 def main():
 
+    # make User
+    user = User(name="",
+                password="")
     GFLEX_API_URL = "localhost"
 
-    client = FlexAPIClient(base_url=GFLEX_API_URL)
-    client.make_request(method="POST", endpoint="/user/login")
-    client.add_plugin(AuthPlugin(app_token="secret"))
-    client.make_request(method="POST", endpoint="/user/login")
-    client.add_plugin(LoggingPlugin())
+    client = FlexAPIClient(user=user, base_url=GFLEX_API_URL)
 
+    response, err = client.make_request(method="POST",
+                                        endpoint="/users/login", 
+                                        data={"email": user.name, "password": user.password})
+    
+    if err:
+          print(err)
+          sys.exit(1)
+
+    print(response.request_response.json())
+    print(response.request_response.status_code)
 
 if __name__ == "__main__":
         main()

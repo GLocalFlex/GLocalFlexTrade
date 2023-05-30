95c4be0872781b5d005425acab23dfd3d589e5b2
stephan mehnert <stephan.mehnert@vtt.fi>
2023-05-30 14:06:15 +0300

    add error handling to rest client

--- src/flxtrd/core/api_client.py
+++ src/flxtrd/core/api_client.py
@@ -1,38 +1,55 @@
-from typing import List, Optional
+from typing import List, Optional, Tuple
 from flxtrd.protocols.base import BaseAPI
-from flxtrd.protocols.rest.rest_api import RestAPI
-from flxtrd.core.plugins.auth import AuthPlugin
+from flxtrd.protocols.restapi import RestAPI
+from flxtrd.core.plugins.auth import AuthPlugin, AuthResponse
 from flxtrd.core.plugins.base import BasePlugin
+from flxtrd.core.types import User, APIResponse
 
 
 class FlexAPIClient:
-    """Example Client that uses the api and auth plugin"""
+    """Example API Client that uses the api and auth plugin
     
-    def __init__(self, base_url: str, protocol: BaseAPI = RestAPI, plugins: Optional[List[BasePlugin]] = None) -> None:
+    Params:
+        user: User object
+        base_url: Base url of the API
+        protocol: Protocol to use for communication with the public API
+        plugins: List of plugins to use
+
+    """
+    
+    def __init__(self, user: User,  base_url: str, protocol: BaseAPI = RestAPI, plugins: Optional[List[BasePlugin]] = None) -> None:
         self.protocol = protocol(base_url=base_url)
         # By default the Auth Plugin is added
-        self.plugins = plugins or [AuthPlugin(app_token="secret")]
+        self.plugins = plugins or [AuthPlugin(user=user,
+                                              authServer=base_url)]
 
     def make_request(self, 
                      method: str,
                      endpoint: str,
                      params: Optional[dict] = None,
-                     data: Optional[dict] = None) -> dict:
-        """Executes all plugins and forwrds the request to the protocol API class"""
+                     ssl: Optional[bool] = False,
+                     data: Optional[dict] = None,
+                     user: User = None) ->  APIResponse:
+        """Executes all plugins and forwards the request to the protocol API class"""
+
+        plugin_data = {}
 
         for _plugin in self.plugins:
-            print("Before request")
-            print(f"Execute plugin {_plugin} ")
-            # _plugin.before_request(endpoint, params=params, data=data)
+            print(f"Execute plugin {_plugin}")
+            plugin_data[f"{str(_plugin)}_before"] =  _plugin.before_request(endpoint, params=params, data=data)
 
-        response = self.protocol.send_request(method, endpoint, params=params, data=data)
+        
+        response, err= self.protocol.send_request(method,
+                                              endpoint,
+                                              params=params,
+                                              data=data,
+                                              ssl=ssl)
 
         for _plugin in self.plugins:
-            print("After request")
-            print(f"Execute plugin {_plugin} ")
-            # _plugin.after_request(response)
+            plugin_data[f"{str(_plugin)}_after"] = _plugin.after_request(response)
 
-        return response
+        return APIResponse(request_response=response, plugin_data=plugin_data or None) , err
+
     
     def add_plugin(self, plugin: BasePlugin):
         """Add a plugin to the list of plugins"""
---
+++ src/flxtrd/protocols/restapi.py
@@ -0,0 +1,43 @@
+import requests
+from typing import List, Optional
+
+from flxtrd.core.plugins.base import BasePlugin
+from flxtrd.protocols.base import BaseAPI
+
+
+
+
+class RestAPI(BaseAPI):
+    """Example REST API class that uses the requests library to make HTTP requests"""
+    
+    def __init__(self, base_url: str):
+        super().__init__(base_url=base_url)
+
+    def send_request(self, method: str,
+                     endpoint: str,
+                     params: Optional[dict] = {},
+                     data: Optional[dict] = {},
+                     headers: Optional[dict] = {},
+                     ssl: bool = True,
+                     **kwargs) -> dict:
+        
+        if ssl:
+            url = f"https://{self.base_url + endpoint}"
+        else:
+            url = f"http://{self.base_url + endpoint}"
+
+        response = requests.request(method, url, headers=headers, params=params, data=data)
+
+
+        return response, self.check_status(response, url, endpoint)
+
+    def check_status(self, response: requests.Response, url, endpoint) -> bool:
+        """Checks the status code of the response"""
+        if response.status_code == 200:
+            return False
+        elif response.status_code == 401:
+            return "Authentication failed"
+        elif response.status_code == 404:
+            return f"{url} not found"
+        elif response.status_code == 500:
+            return f"""Internal server error, endpoint is correct but data or parameters might be wrong. Check the API documentation for endpoint {endpoint} """    
